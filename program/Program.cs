c7b1bdba2acc4b6f6fc8099e2ac201c9a1ca9677
Alla <100729027+allaberezina85@users.noreply.github.com>
2024-04-11 10:33:02 +0300

    Update Program.cs

--- program/Program.cs
+++ program/Program.cs
@@ -1,7 +1,11 @@
-﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
+
+using System.Text;
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
 //печать элементов массива в строку через пробел
 void PrintArray(string [] mas)
 {
@@ -19,7 +23,7 @@ string [] FillArray(int num)
     string[] mas = new string[num];
     for (int i = 0; i < mas.Length; i++)
     {
-        Console.Write("Элемент массива: ");
+        Console.WriteLine("Введите элемент массива: ");
         mas[i] = Console.ReadLine()!;
     }
     return mas;
@@ -61,16 +65,35 @@ string [] NewArrayWithLength3(string [] arrey)
 }
 
 //основная программа
+int num=0;
 Console.WriteLine("Введите размер массива: ");
-int num=Convert.ToInt32(Console.ReadLine());
+try
+{
+    num=Convert.ToInt32(Console.ReadLine());
 
-string [] arrey=FillArray(num);
+}
+catch (ArgumentException e)
+{
+    Console.WriteLine($"Processing failed: {e.Message}");
+}
+if (num!=0)
+{
+    string [] arrey=FillArray(num);
 
+    Console.WriteLine("Исходный массив: ");
+    PrintArray(arrey);
 
-Console.WriteLine("Исходный массив: ");
-PrintArray(arrey);
+    
+    string [] arreyNew=NewArrayWithLength3(arrey);
+    if (arreyNew.Length!=0)
+    {
 
-Console.WriteLine("Новый массив с элементами длиной не больше 3 символов: ");
-string [] arreyNew=NewArrayWithLength3(arrey);
-PrintArray(arreyNew);
+    Console.WriteLine("Новый массив с элементами длиной не больше 3 символов: ");
+    PrintArray(arreyNew);
+    }
+    else
+     Console.WriteLine("В исходном массиве нет элементов длиной не больше 3 символов. ");
+   
 
+}
+ 
